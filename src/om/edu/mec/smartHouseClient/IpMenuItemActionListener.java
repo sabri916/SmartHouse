27ab7f4ddea3302e2adc2e71e5e3ce193a1cc600
sabri916 <sbrkrs@gmail.com>
2015-12-27 21:04:46 +0400

    updated classpath arguments to build commands

---
+++ src/om/edu/mec/smartHouseClient/IpMenuItemActionListener.java
@@ -0,0 +1,11 @@
+package om.edu.mec.smartHouseClient;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class IpMenuItemActionListener implements ActionListener {
+	
+	public void actionPerformed(ActionEvent e){
+		JOptionPane.showMessageDialog(null, "I am happy.");
+	}
+}
